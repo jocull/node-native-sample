328c8e67002c44524e8b0c3a90e31ec6dc905ccd
James O'Cull <jocull@delmarsd.com>
2016-05-31 23:47:35 -0400

    Added blocking / non-blocking example

--- index.js
+++ index.js
@@ -2,10 +2,25 @@
 
 const addon = require('./build/Release/addon');
 
+// The blocking way
 for (let x = 0; x < 10; x++) {
     let y = x;
-    console.log('hello...');
+    let a = ~~(Math.random() * 14620 + 1);
+    console.log('Hello #' + y);
+    for (let z = 0; z < 400000000; z++) {
+        a++;
+    }
+    console.log("Instance " + y + ": " + "world! Enjoy your number: " + ~~(a % 14620));
+}
+console.log('Done with the loop!');
+
+
+// The non-blocking way
+for (let x = 0; x < 10; x++) {
+    let y = x;
+    console.log('Hello #' + y);
     addon.hello((err, result) => {
         console.log("Instance " + y + ": " + result);
     });
 }
+console.log('Done with the loop!');
